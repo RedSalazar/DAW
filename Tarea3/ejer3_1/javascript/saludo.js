85cfbaba23afc26ae9a57231c005e9c5d78f8a91
RedSala <davsalazar3@gmail.com>
2021-09-17 20:59:09 +0200

    ejer3.1

---
+++ Tarea3/ejer3_1/javascript/saludo.js
@@ -0,0 +1,9 @@
+
+console.log("Saludos");
+
+
+// # Crea un directorio para la práctica 3.1
+// ## Crea una página llamada index.html y enlaza esta página con el archivo saludo.js 
+// ## Crea una subcarpeta js y crea un archivo saludo.js. 
+// ## En el archivo saludo.js debe mostrar un mensaje por la consola
+// ## Ejecuta este código tanto en el navegador como con node.js
